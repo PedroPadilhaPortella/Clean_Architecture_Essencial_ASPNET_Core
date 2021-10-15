563be709bc2bc0a7f6bc00a0358ca551942d3303
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-10-15 01:57:56 -0300

    interface web de produtos

--- CleanArchMVC/CleanArchMVC.Application/DTO/ProductDTO.cs
+++ CleanArchMVC/CleanArchMVC.Application/DTO/ProductDTO.cs
@@ -13,28 +13,32 @@ namespace CleanArchMVC.Application.DTO
         [Required(ErrorMessage = "The Name is Required")]
         [MinLength(3)]
         [MaxLength(100)]
+        [DisplayName("Name")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "The Description is Required")]
         [MinLength(5)]
         [MaxLength(200)]
-        public string Description { get; private set; }
+        [DisplayName("Description")]
+        public string Description { get; set; }
 
         [Required(ErrorMessage = "The Price is Required")]
         [Column(TypeName="decimal(18,2)")]
         [DataType(DataType.Currency)]
         [DisplayName("Price")]
-        public decimal Price { get; private set; }
+        public decimal Price { get; set; }
 
         [Required(ErrorMessage = "The Stock is Required")]
         [Range(1, 9999)]
         [DisplayName("Stock")]
-        public int Stock { get; private set; }
+        public int Stock { get; set; }
 
         [MaxLength(250)]
         [DisplayName("Product Image")]
-        public string Image { get; private set; }
+        public string Image { get; set; }
 
+        [Required(ErrorMessage = "The Category is Required")]
+        [DisplayName("Category")]
         public int CategoryId { get; set; }
 
         [DisplayName("Categories")]
--- CleanArchMVC/CleanArchMVC.WebUI/Controllers/ProductsController.cs
+++ CleanArchMVC/CleanArchMVC.WebUI/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using CleanArchMVC.Application.DTO;
 using CleanArchMVC.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,9 +11,11 @@ namespace CleanArchMVC.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -19,6 +23,50 @@ namespace CleanArchMVC.WebUI.Controllers
         {
             IEnumerable<ProductDTO> products = await _productService.GetAll();
             return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Categories = new SelectList(await _categoryService.GetAll(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Categories = new SelectList(await _categoryService.GetAll(), "Id", "Name");
+            return View(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDTO = await _productService.GetById(id);
+
+            if (productDTO == null) return NotFound();
+
+            IEnumerable<CategoryDTO> categories = await _categoryService.GetAll();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name", productDTO.CategoryId);
+
+            return View(productDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDTO);
         }
     }
 }
